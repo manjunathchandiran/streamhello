09761a6d6f4bd299f362da150bbc32b784280ebc
manjunathchandiran <manjunathchandiran@gmail.com>
2024-12-13 15:24:10 +0530

    Create streamhello1.py

---
+++ streamhello1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+# Show the content page after successful login
+st.title("Secure Streamlit App")
+st.write("Welcome to the app!")
+st.write("Hello!")
+# You can add more content here as needed
