94ad68316d693a90927425af64d5f7331d22bc10
manjunathchandiran <manjunathchandiran@gmail.com>
2025-02-26 18:05:15 +0530

    Update streamhello1.py

--- streamhello1.py
+++ streamhello1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import warnings
 import datetime
 from datetime import datetime
